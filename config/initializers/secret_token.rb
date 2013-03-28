2c75fbba2a9b02e482aa60ceeaf8a6d6910ccfde
wan <wantingyi@sina.com>
2013-03-27 22:08:50 -0700

    create project
    start

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+App2::Application.config.secret_token = '36364b846bc2e3017567dc08046d1d3d89a32234aa88f12501a8b6d44236d979fe3be031206bc15340625ba6b56c8e8807aa3e644eacc6e52434a4de30444ca7'
